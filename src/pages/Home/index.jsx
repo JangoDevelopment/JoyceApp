696baa021daf939beed80555656919c60de1c454
Joao Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-07-29 19:57:20 -0300

    shit commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
-import Form from "../../components/form_contatos"
+import Form from "../../components/form_contatos";
 
 const Home = () => {
-
     return (
         <div>
             <h1>Home</h1>
-            <Form titulo="Vamos tomar um café?"/> 
+            <Form titulo="Vamos tomar um café?" />
         </div>
     );
 };
