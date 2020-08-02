7c39fb828aced2763306aa10ce6c8a1cdf5984ce
Joao Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-08-01 21:55:28 -0300

    portifolio added + responsive design

--- src/components/portifolio/index.jsx
+++ src/components/portifolio/index.jsx
@@ -6,24 +6,27 @@ const PortifolioComponent = (props) => {
         <div className="portifolio">
             <div className="parte1 flex">
                 <div className="section flex-column">
-                    <div className="box block_1">
-                        <h3 className="title">projetos</h3>
+                    <div className="box flex block_1">
+                        <img className="unselectable" src="./images/portifolio/1.png" alt="" />
+                        <h3 className="title unselectable">projetos</h3>
                     </div>
                     <div className="description">
                         <p>Você tem um sonho e não sabe por onde começar? Comece por um projeto que tenha a sua ...</p>
                     </div>
                 </div>
                 <div className="section flex-column">
-                    <div className="box block_2">
-                        <h3 className="title">reformas</h3>
+                    <div className="box flex block_2">
+                        <img className="unselectable" src="./images/portifolio/2.png" alt="" />
+                        <h3 className="title unselectable">reformas</h3>
                     </div>
                     <div className="description">
                         <p>Precisando de uma cara nova pro seu espaço?--- Se seu ambiente/imóvel está precisando ser adaptado...</p>
                     </div>
                 </div>
                 <div className="section flex-column">
-                    <div className="box block_3">
-                        <h3 className="title">acessoria de obras</h3>
+                    <div className="box flex block_3">
+                        <img className="unselectable" src="./images/portifolio/3.png" alt="" />
+                        <h3 className="title unselectable">acessoria de obras</h3>
                     </div>
                     <div className="description">
                         <p>Trabalhamos com visitas técnicas e cronogramas de obras. Acreditamos que o sucesso de uma obra...</p>
