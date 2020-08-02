0213b84a6e49a7c83b51e66ae8f805a5a6b91565
Joao Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-08-01 23:30:42 -0300

    changed name

---
+++ build/precache-manifest.2405a3dd41150735cbfe6ba18826c8e9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e266bc706d8ea0980246dc4523e334",
+    "url": "/joyceapp/index.html"
+  },
+  {
+    "revision": "2f7ccda126278c95c9f4",
+    "url": "/joyceapp/static/css/main.c6a91778.chunk.css"
+  },
+  {
+    "revision": "cda58d3e0f5f01917ab4",
+    "url": "/joyceapp/static/js/2.840c3f6c.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/joyceapp/static/js/2.840c3f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7ccda126278c95c9f4",
+    "url": "/joyceapp/static/js/main.d700db33.chunk.js"
+  },
+  {
+    "revision": "9b5bc695f109a66fc9f7",
+    "url": "/joyceapp/static/js/runtime-main.85de0b93.js"
+  },
+  {
+    "revision": "4afbf01ee1dc26f6b767053576f19839",
+    "url": "/joyceapp/static/media/0.4afbf01e.jpg"
+  },
+  {
+    "revision": "5b98b84379829c54829ba8f699b3d2ef",
+    "url": "/joyceapp/static/media/1.5b98b843.jpg"
+  },
+  {
+    "revision": "907db66a2fe70394d39c359e021ac83f",
+    "url": "/joyceapp/static/media/1.907db66a.png"
+  },
+  {
+    "revision": "b67d6b88775d196e77c85694c7a79ea6",
+    "url": "/joyceapp/static/media/2.b67d6b88.jpg"
+  },
+  {
+    "revision": "c079f15fecd35895653e84633962f068",
+    "url": "/joyceapp/static/media/2.c079f15f.jpg"
+  },
+  {
+    "revision": "bb72b3d932ab2c35a645ac0c875ff1c4",
+    "url": "/joyceapp/static/media/3.bb72b3d9.jpg"
+  },
+  {
+    "revision": "4ff6d6f697cc2676a514c1fc98bf1a69",
+    "url": "/joyceapp/static/media/b1.4ff6d6f6.png"
+  },
+  {
+    "revision": "7e271eceb84a23bfb6afb2d1232aaecb",
+    "url": "/joyceapp/static/media/b2.7e271ece.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/JoyceApp/precache-manifest.37b2ab4ceb8eb3aef310594bdfe639b1.js"
+  "/joyceapp/precache-manifest.2405a3dd41150735cbfe6ba18826c8e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JoyceApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/joyceapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
