d705183790439369f421776e5bc8fc272145c39c
Joao Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-08-31 02:35:34 -0300

    added component plus documentation in the archieve

--- src/components/portifolio/index.jsx
+++ src/components/portifolio/index.jsx
@@ -2,19 +2,26 @@ import React from "react";
 import img1 from "./images/1.png";
 import img2 from "./images/2.png";
 import img3 from "./images/3.png";
+import { Link, animateScroll as scroll } from "react-scroll";
+import PortifolioSection from "../portifolio_section";
 import "./styles.css";
 
 const gallery = [{ name: "project 0" }, { name: "project 2" }, { name: "project 3" }, { name: "project 4" }, { name: "project 5" }, { name: "project 6" }];
 
 const PortifolioComponent = (props) => {
+    const description_residencial =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in sem pulvinar, interdum odio quis, sollicitudin sapien. Donec ullamcorper convallis urna, eu gravida nulla faucibus tristique. Nunc mollis pulvinar purus, sit amet aliquam nulla dignissim et. Duis posuere, enim ac tempor venenatis, orci eros blandit nibh, sed sollicitudin ante erat id lacus.";
+
     return (
         <div className="portifolio">
             <div className="parte1 flex">
                 <div className="section flex-column">
-                    <div className="box flex block_1">
-                        <img className="unselectable box_img" src={img1} alt="" />
-                        <h3 className="title unselectable">projetos</h3>
-                    </div>
+                    <Link activeClass="active" className="test1" to="test1" spy={true} smooth={true} duration={500}>
+                        <div className="box flex block_1">
+                            <img className="unselectable box_img" src={img1} alt="" />
+                            <h3 className="title unselectable">projetos</h3>
+                        </div>
+                    </Link>
                     <div className="description">
                         <p>Você tem um sonho e não sabe por onde começar? Comece por um projeto que tenha a sua ...</p>
                     </div>
@@ -38,55 +45,40 @@ const PortifolioComponent = (props) => {
                     </div>
                 </div>
             </div>
-            <div className="projetos flex-column">
+            <div name="test1" className="projetos flex-column">
                 <h1 className="section_title">Projetos</h1>
                 <div className="parte1 flex">
                     <div className="section flex-column">
-                        <div className="box flex block_1">
-                            <h3 className="title unselectable">residencial</h3>
-                        </div>
+                        <Link activeClass="active" className="test1" to="residencial" spy={true} smooth={true} duration={500}>
+                            <div className="box flex block_1">
+                                <h3 className="title unselectable">residencial</h3>
+                            </div>
+                        </Link>
                     </div>
                     <div className="section flex-column">
-                        <div className="box flex block_2">
-                            <h3 className="title unselectable">comercial</h3>
-                        </div>
+                        <Link activeClass="active" className="test1" to="comercial" spy={true} smooth={true} duration={500}>
+                            <div className="box flex block_2">
+                                <h3 className="title unselectable">comercial</h3>
+                            </div>
+                        </Link>
                     </div>
                     <div className="section flex-column">
-                        <div className="box flex block_3">
-                            <h3 className="title unselectable">interiores</h3>
-                        </div>
+                        <Link activeClass="active" className="test1" to="interiores" spy={true} smooth={true} duration={500}>
+                            <div className="box flex block_3">
+                                <h3 className="title unselectable">interiores</h3>
+                            </div>
+                        </Link>
                     </div>
                 </div>
 
-                <div className="subsection">
-                    <h2 className="subsection_title">residencial</h2>
-                    <div className="project_galery flex">
-                        {gallery.map((item, i) => (
-                            <div className="project">
-                                <h3 className="project_name unselectable">{item.name}</h3>
-                            </div>
-                        ))}
-                    </div>
+                <div name="residencial" className="subsection">
+                    <PortifolioSection section_title={"residencial"} section_list={gallery} section_description={description_residencial} />
                 </div>
-                <div className="subsection">
-                    <h2 className="subsection_title">comercial</h2>
-                    <div className="project_galery flex">
-                        {gallery.map((item, i) => (
-                            <div className="project">
-                                <h3 className="project_name unselectable">{item.name}</h3>
-                            </div>
-                        ))}
-                    </div>
+                <div name="comercial" className="subsection">
+                    <PortifolioSection section_title={"comercial"} section_list={gallery} section_description={description_residencial} />
                 </div>
-                <div className="subsection">
-                    <h2 className="subsection_title">interiores</h2>
-                    <div className="project_galery flex">
-                        {gallery.map((item, i) => (
-                            <div className="project">
-                                <h3 className="project_name unselectable">{item.name}</h3>
-                            </div>
-                        ))}
-                    </div>
+                <div name="interiores" className="subsection">
+                    <PortifolioSection section_title={"interiores"} section_list={gallery} section_description={description_residencial} />
                 </div>
             </div>
             <div className="reformas">
---
+++ src/components/portifolio_section/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./styles.css";
+
+/**
+ *
+ * @param {string} section_title O titulo que sera escrrito em maior fonte no bloco
+ * @param {string} section_description O texto que virá abaixo do titulo explicando um pouco sobre o bloco
+ * @param {object} section_list um objeto que contenha todos os projetos dessa sessão
+ *
+ */
+
+const PortifolioSection = ({ section_title, section_description, section_list }) => {
+    return (
+        <div>
+            <h2 className="subsection_title">{section_title}</h2>
+            <h4 className="subsection_description">{section_description}</h4>
+            <div className="project_galery flex">
+                {section_list.map((item, i) => (
+                    <div key={i} className="project">
+                        <h3 className="project_name unselectable">{item.name}</h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default PortifolioSection;
