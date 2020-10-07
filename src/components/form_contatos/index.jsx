93fe623ee7236211aab1aa31d9f25463dd29906e
Joao Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-10-06 22:56:33 -0300

    changed contact page + added mobile vision

--- src/components/form_contatos/index.jsx
+++ src/components/form_contatos/index.jsx
@@ -2,29 +2,24 @@ import React, { useState } from "react";
 import "./styles.css";
 
 const FormContatos = (props) => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert("Mensagem enviada com sucesso!");
-        setName("");
-        setEmail("");
-        setMessage("");
-    };
-
     return (
         <div className="contato_form">
-            <form className="form flex-column" onSubmit={handleSubmit}>
+            <form className="form flex-column">
                 <h1>vamos tomar um café?</h1>
-                <h2>Nome</h2>
-                <input type="text" required value={name} onChange={(e) => setName(e.target.value)} />
-                <h2>Email</h2>
-                <input type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
-                <h2>Mensagem</h2>
-                <textarea name="" id="" cols="30" rows="5" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-                <input className="submit" type="submit" value="Enviar" />
+                <h2>(12) 98147 0468</h2>
+                <h2>arq.joycecacador@gmail.com</h2>
+                <h3>me encontre aqui</h3>
+                <p>Rua Monsenhor Aníbal</p>
+                <p>de Melo, 206 - Pedregulho</p>
+                <iframe
+                    className="map"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d546.7256889675095!2d-45.20377815729114!3d-22.80552525712058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ccc44181e159f3%3A0x760a98e63f69e0e1!2sR.%20Monsenhor%20An%C3%ADbal%20de%20Melo%2C%20206%20-%20Pedregulho%2C%20Guaratinguet%C3%A1%20-%20SP%2C%2012511-050!5e0!3m2!1spt-BR!2sbr!4v1602034778017!5m2!1spt-BR!2sbr"
+                    width={500}
+                    height={200}
+                    frameborder="0"
+                    allowfullscreen=""
+                    aria-hidden="false"
+                    tabindex="0"></iframe>
             </form>
             <div className="background" />
         </div>
