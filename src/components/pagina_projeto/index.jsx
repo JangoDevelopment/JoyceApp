a59afb4723fda03a36d408ee61731938abaa9686
Joao Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-10-21 21:57:04 -0300

    informacoes adicionadas na pagina de projetos de acordo com o projeto

--- src/components/pagina_projeto/index.jsx
+++ src/components/pagina_projeto/index.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import "./styles.css";
 
-const info = {
-    titulo: "Projeto X",
-    ano: "2020",
-    local: "São José dos Campos",
-    capa: "images/capa.jpg",
-    descricao:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus commodo non erat in ultrices. Maecenas ultricies maximus risus et posuere. Vivamus non dolor luctus, interdum orci ac, suscipit lectus. Vivamus a nunc ut leo faucibus viverra. Mauris sed vulputate nisl. Aliquam et ex ac justo tristique scelerisque vitae sagittis purus. Donec aliquam, nunc sed elementum ultricies, sapien justo condimentum nulla, sed convallis nulla nunc at ligula. Nullam lacus diam, pretium vitae pellentesque eu, tempor at enim. Fusce vulputate condimentum metus vel feugiat. Donec consequat odio ex, sed elementum magna fringilla vel. Integer iaculis odio quis elit rutrum maximus. Pellentesque vulputate dignissim fringilla. Duis non lectus dictum, pellentesque libero ut, fringilla orci. Nunc mattis lacus eget sodales venenatis. Donec in ante felis.",
-};
+import projetos from "../../data/projetos";
 
 const PaginaComponente = (props) => {
+    const urlParams = window.location.hash;
+    const splicedParams = urlParams.split("/", 4);
+
+    const categoria = projetos[`${splicedParams[2]}`];
+
+    const projeto = categoria.find((x) => x.id === parseInt(splicedParams[3]));
+
+    console.log("here", projeto);
+
     return (
         <div className="projeto_component flex-column">
-            <h1 className="titulo">{info.titulo}</h1>
+            <h1 className="titulo">{projeto.titulo}</h1>
             <div className="banner_principal flex">
-                <img src={require(`../../${info.capa}`)} className="img_principal" />
+                <img src={require(`../../${projeto.capa}`)} className="img_principal" />
                 <div className="description">
-                    <p className="texto">{info.descricao}</p>
+                    <p className="texto">{projeto.descricao}</p>
                 </div>
             </div>
             <div className="additional_info flex">
                 <div className="bloco entrega flex">
                     <p className="bloco_titulo">Ano do Projeto</p>
-                    <p className="bloco_info">{info.ano}</p>
+                    <p className="bloco_info">{projeto.ano}</p>
                 </div>
                 <div className="bloco local flex">
                     <p className="bloco_titulo">Local</p>
-                    <p className="bloco_info">{info.local}</p>
+                    <p className="bloco_info">{projeto.local}</p>
                 </div>
             </div>
         </div>
