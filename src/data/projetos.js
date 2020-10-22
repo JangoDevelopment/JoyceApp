edd22c949c1c636653c7b17ace21059b734fe42e
Joao Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-10-21 22:11:23 -0300

    imagens adicionadas nas páginas dos projetos

--- src/data/projetos.js
+++ src/data/projetos.js
@@ -7,7 +7,7 @@ export default {
             ano: 2019,
             descricao:
                 "Esse projeto contempla  uma casa pequena  para um jovem casal. A conexão entre varanda, sala, cozinha e área de serviço proporciona amplitude ao ambiente e maior praticidade. O banheiro foi posicionado de forma que pudesse ser tanto privativo ao quarto quanto para uso das áreas sociais.",
-            capa: "images/capa.jpg",
+            capa: "images/capas/residencial_1.jpg",
         },
     ],
     comercial: [
@@ -18,7 +18,7 @@ export default {
             ano: 2019,
             descricao:
                 "Essa escola de educação infantil segue uma metodologia acolhedora que estimula a fantasia, o convívio coletivo e o livre brincar. Os ambientes precisavam apresentar o raciocínio dessa metodologia, com uma linguagem limpa e lúdica para o melhor aprendizado das crianças. ",
-            capa: "images/capa.jpg",
+            capa: "images/capas/comercial_1.jpg",
         },
         {
             id: 2,
@@ -27,7 +27,7 @@ export default {
             ano: 2019,
             descricao:
                 "Essa loja precisava de um novo lugar para expor seu produtos. O espaço era pequeno, logo o desafio foi otimizar o máximo possível. De um lado trabalhamos com uma nova área de exposição para os produtos menores, um balcão de atendimento que dá continuidade a mesa de escritório. De outro, prateleiras para produtos maiores , painel de televisores e câmeras. ",
-            capa: "images/capa.jpg",
+            capa: "images/capas/comercial_2.jpg",
         },
         {
             id: 3,
@@ -36,7 +36,7 @@ export default {
             ano: 2020,
             descricao:
                 "Galpão Comercial Multifuncional. Parceria realizada com a MS Estruturas Metálicas. A estrutura do galpão é realizada em pilares de concreto pré-fabricado, estrutura da cobertura em treliças metálicas e vedação em bloco de concreto. O uso desse materiais otimiza e acelera a execução da obra. ",
-            capa: "images/capa.jpg",
+            capa: "images/capas/comercial_1.jpg",
         },
     ],
     interiores: [
@@ -47,7 +47,7 @@ export default {
             ano: 2020,
             descricao:
                 "O clientes queriam dar uma cara nova para essa  Sala de Estar/Tv, mas não queriam nada de “quebra – quebra”. O painel em madeira era um item que deveria continuar no ambiente. Mudamos os móveis e o layout da sala. Demos novas cores e texturas paras as paredes e finalizamos com uma sanca de gesso e uma iluminação aconchegante. Confira como ficou o resultado! ",
-            capa: "images/capa.jpg",
+            capa: "images/capas/interiores_1.png",
         },
     ],
     reformas: [
@@ -58,7 +58,7 @@ export default {
             ano: 2019,
             descricao:
                 "A casa MC foi construída há mais de 60 anos. Os cômodos eram mal divididos, as paredes precisavam de reboco novo, o piso era de chão batido, entre outras coisas. O Desafio foi transformar essa casa em uma casa atual com ambientes integrados, suíte e quartos confortáveis. Veja fotos do antes e depois.",
-            capa: "images/capa.jpg",
+            capa: "images/capas/reformas_1.png",
         },
         {
             id: 2,
@@ -67,7 +67,7 @@ export default {
             ano: 2020,
             descricao:
                 "Depois de quase 20 anos com uma casa agitada e uma cozinha pequenina, os clientes resolveram ampliar esse ambiente que era centro de todas as reuniões. Agora a nova cozinha possuí bancadas e armários funcionais, perfeito para as reuniões familiares.",
-            capa: "images/capa.jpg",
+            capa: "images/capas/comercial_1.jpg",
         },
     ],
     obras: [
@@ -78,7 +78,7 @@ export default {
             ano: 2019,
             descricao:
                 "A casa MC foi construída há mais de 60 anos. Os cômodos eram mal divididos, as paredes precisavam de reboco novo, o piso era de chão batido, entre outras coisas. O Desafio foi transformar essa casa em uma casa atual com ambientes integrados, suíte e quartos confortáveis. Veja fotos do antes e depois.",
-            capa: "images/capa.jpg",
+            capa: "images/capas/comercial_1.jpg",
         },
         {
             id: 2,
@@ -87,7 +87,7 @@ export default {
             ano: 2020,
             descricao:
                 "Depois de quase 20 anos com uma casa agitada e uma cozinha pequenina, os clientes resolveram ampliar esse ambiente que era centro de todas as reuniões. Agora a nova cozinha possuí bancadas e armários funcionais, perfeito para as reuniões familiares.",
-            capa: "images/capa.jpg",
+            capa: "images/capas/comercial_1.jpg",
         },
     ],
 };
