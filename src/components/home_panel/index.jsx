9aff62b7d5a741834658520dd5d5bc1f4e9af71a
gasantos <gasantos@nelogica.com.br>
2022-06-27 19:23:21 -0300

    First try of carousel change, the logo in carousel its changing too.

--- src/components/home_panel/index.jsx
+++ src/components/home_panel/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles.css";
+import logo_img from "../../assets/logo.png";
 
 import styled from "styled-components";
 import img0 from "./images/0.jpg";
@@ -7,21 +8,7 @@ import img1 from "./images/1.png";
 import img2 from "./images/2.jpg";
 import img3 from "./images/3.jpg";
 
-import b1 from "./images/b1.png";
-import b2 from "./images/b2.png";
-import b3 from "./images/b3.png";
-import b4 from "./images/b4.png";
-
-
-
 const HomePanel = ({ painel }) => {
-    const titulos = [
-        { id: 0, content: "desenhar sonhos", color: "#FEC90E" },
-        { id: 1, content: "planejamento", color: "#EA7979" },
-        { id: 2, content: "você conectado com seu lar", color: "#609BA3" },
-        { id: 3, content: "funcionalidade", color: "#50967F" },
-    ];
-
     const Image0 = styled.div`
         background-image: url(${img0});
     `;
@@ -39,45 +26,22 @@ const HomePanel = ({ painel }) => {
         background-image: url(${img3});
     `;
 
-    const Title = styled.h2`
-        font-family: "Poppins", sans-serif;
-        font-weight: 600;
-        font-size: 86px;
-        color: ${titulos[painel].color};
-    `;
-
     return (
         <div className="home_panel">
             <div className="background">
-                <div className="overlay flex-column">
-                    <h1 className="unselectable">arquitetura é</h1>
-                    <Title className="unselectable titulo">{titulos[painel].content}</Title>
-                </div>
-
                 {painel === 0 ? (
-                    <div>
-                        <img className="b1" src={b1} alt="" />
-                        <Image0 className="image" />
-                    </div>
+                    <Image0 className="image" />
                 ) : null}
                 {painel === 1 ? (
-                    <div>
-                        <img className="b2" src={b2} alt="" />
-                        <Image1 className="image" />
-                    </div>
+                    <Image1 className="image" />
                 ) : null}
                 {painel === 2 ? (
-                    <div>
-                        <img className="b3" src={b3} alt="" />
-                        <Image2 className="image" />
-                    </div>
+                    <Image2 className="image" />
                 ) : null}
                 {painel === 3 ? (
-                    <div>
-                        <img className="b4" src={b4} alt="" />
-                        <Image3 className="image" />
-                    </div>
+                    <Image3 className="image" />
                 ) : null}
+                <img className="logo-carousel-img" src={logo_img} alt="" />
             </div>
         </div>
     );
