0f3e3c7c9386e7a9f856bef3199caa506735f1f2
gasantos <gasantos@nelogica.com.br>
2022-08-27 12:11:25 -0300

    Adds new images to home_panel component and in presentation

--- src/components/home_panel/index.jsx
+++ src/components/home_panel/index.jsx
@@ -6,7 +6,6 @@ import styled from "styled-components";
 import img0 from "./images/0.jpg";
 import img1 from "./images/1.png";
 import img2 from "./images/2.jpg";
-import img3 from "./images/3.jpg";
 
 const HomePanel = ({ painel }) => {
     const Image0 = styled.div`
@@ -14,16 +13,11 @@ const HomePanel = ({ painel }) => {
     `;
 
     const Image1 = styled.div`
-        background-color: #fff0f0;
         background-image: url(${img1});
     `;
 
     const Image2 = styled.div`
         background-image: url(${img2});
-    `;
-
-    const Image3 = styled.div`
-        background-image: url(${img3});
     `;
 
     return (
@@ -37,9 +31,6 @@ const HomePanel = ({ painel }) => {
                 ) : null}
                 {painel === 2 ? (
                     <Image2 className="image blue-image" />
-                ) : null}
-                {painel === 3 ? (
-                    <Image3 className="image green-image" />
                 ) : null}
                 {/* <div className="logo-carousel-container">
                     <img className="logo-carousel-img" src={logo_img} alt="" />
